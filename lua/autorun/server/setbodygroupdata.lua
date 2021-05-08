2efa3ab7f87cd996d49bddd37cd83284a6b81505
Python1320 <python1320@users.noreply.github.com>
2021-05-08 21:51:31 +0300

    Allow up to 2^32 bits of bodygroup data

--- lua/autorun/server/setbodygroupdata.lua
+++ lua/autorun/server/setbodygroupdata.lua
@@ -3,8 +3,8 @@ local Tag = 'BodyGroupData'
 util.AddNetworkString(Tag)
 
 net.Receive(Tag, function(len, pl)
-	local n = net.ReadUInt(24)
-	n=n>2^24 and 2^24 or n
+	local n = net.ReadUInt(32)
+	n=n>2^32 and 2^32 or n
 	if hook.Run("SetBodyGroupData",pl,n)==false then return end
 	pl:SetBodyGroupData(n)
 end)
--- lua/autorun/setbodygroupdata.lua
+++ lua/autorun/setbodygroupdata.lua
@@ -2,14 +2,14 @@ local Tag = 'BodyGroupData'
 
 
 FindMetaTable"Player".SetBodyGroupData = SERVER and function(self, n)
-	n=n>2^24 and 2^24 or n<0 and 0 or n
+	n=n>2^32 and 2^32 or n<0 and 0 or n
 	self:SetSaveValue("SetBodyGroup", n)
 end or function(self, n)
-	n=n>2^24 and 2^24 or n<0 and 0 or n
+	n=n>2^32 and 2^32 or n<0 and 0 or n
 	if self ~= LocalPlayer() then return end
 	self[Tag] = n
 	net.Start(Tag)
-		net.WriteUInt(n or 0, 24)
+		net.WriteUInt(n or 0, 32)
 	net.SendToServer()
 end
 
